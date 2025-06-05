b32abe35e59bf0ccb0ce9f4689f7ea0401935513
jvbm2177@gmail.com <jvbm2177@gmail.com>
2025-06-05 13:51:07 -0300

    agora ele chama certinho

--- src/main/java/com/KafkaUserService/api/Kafka/UserConsumer.java
+++ src/main/java/com/KafkaUserService/api/Kafka/UserConsumer.java
@@ -1,5 +1,7 @@
 package com.KafkaUserService.api.Kafka;
 
+import com.KafkaUserService.api.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import com.KafkaUserService.api.models.User;
@@ -7,12 +9,15 @@ import com.KafkaUserService.api.models.User;
 @Component
 public class UserConsumer {
 
+    @Autowired
+    EmailService emailService;
+
     //aqui é o consumer, que le essa mensagem do quadro, que é quando o user é criado
     @KafkaListener(topics = "user-criado", groupId = "grupo-email")
     //esse topico seria como o titulo do "PostIt"
     //e esse groupId seria como o sub-titulo
     public void consumir(User user) {
         System.out.println("Usuário criado! Enviando e-mail para: " + user.getEmail());
-        // aqui eu chamaria o email service
+        emailService.enviarEmailConfirmacao(user.getEmail(), user.getName());
     }
 }
